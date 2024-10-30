32f8022542fe0039eea0f4948eeabdd1612aaaea
kevinongg <kevinongg@gmail.comm>
2024-10-30 02:43:43 -0400

    cleaned and  fixed code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-function App() {
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
-
+const App = () => {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  
+  
   return (
     <>
-
+    {
+      puppies.map((puppy) => {
+        return 
+          <p onClick={()=>{}}  key={puppy.id}>{puppy.name} </p>
+          
+      })
+    }
+    
     </>
   )
 }
